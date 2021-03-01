b5809f65f54584fc899c2f9d791766cdbd782727
shaneboulden <shane.boulden@gmail.com>
2021-03-02 07:27:01 +1000

    change text

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -20,7 +20,7 @@ const Home = () => {
         className="text-input-field"
       />
       <Link to={`/${roomName}`} className="enter-room-button">
-        Join room
+        Join channel
       </Link>
     </div>
   );
